1d4ae7fb2322dcd7be8e6bbd7d5b0a0563dd7149
R7Frank <r7frank@hotmail.com>
2023-12-28 03:25:07 -0300

    ProductRepository criado e feito o seu primeiro teste

--- src/main/java/com/r7frank/dscommerce/controllers/ProductControllers.java
+++ src/main/java/com/r7frank/dscommerce/controllers/ProductControllers.java
@@ -1,16 +1,27 @@
 package com.r7frank.dscommerce.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.r7frank.dscommerce.entities.Product;
+import com.r7frank.dscommerce.repositories.ProductRepository;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductControllers {
 
+	@Autowired
+	private ProductRepository repository;
+	
 	@GetMapping()
-	public String Teste() {
-		return "Spring Rest está no ar!";
+	public String teste() {
+		Optional<Product> result = repository.findById(1L);
+		Product product = result.get();
+		return product.getName();
 	}
 	
 	
